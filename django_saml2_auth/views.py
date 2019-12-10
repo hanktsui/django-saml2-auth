89d879e9d5e4bfe6def72817f373610792418f49
H.C. Tsui <henrytsui@gmail.com>
2019-12-09 19:13:30 -0800

    modified to fix next_url issue

--- django_saml2_auth/views.py
+++ django_saml2_auth/views.py
@@ -158,6 +158,7 @@ def acs(r):
     saml_client = _get_saml_client(get_current_domain(r))
     resp = r.POST.get('SAMLResponse', None)
     next_url = r.session.get('login_next_url', _default_next_url())
+    next_url = r.POST.get('RelayState', next_url)
 
     if not resp:
         return HttpResponseRedirect(get_reverse([denied, 'denied', 'django_saml2_auth:denied']))
@@ -247,8 +248,8 @@ def signin(r):
     r.session['login_next_url'] = next_url
 
     saml_client = _get_saml_client(get_current_domain(r))
-    _, info = saml_client.prepare_for_authenticate()
-
+#    _, info = saml_client.prepare_for_authenticate()
+    _, info = saml_client.prepare_for_authenticate(relay_state=next_url)
     redirect_url = None
 
     for key, value in info['headers']:
